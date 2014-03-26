2645cf8bdaaaa8565758cc14b20040b3f5139cdb
mawi-develop <marcin.wicha@interia.eu>
2014-03-26 12:07:07 +0100

    First fb test

--- index.php
+++ index.php
@@ -1,3 +1,50 @@
-<body>
-FB Test
-</body>
+<?php
+  // Remember to copy files from the SDK's src/ directory to a
+  // directory in your application on the server, such as php-sdk/
+  require_once('php-sdk/facebook.php');
+
+  $config = array(
+    'appId' => '306020839546686',
+    'secret' => '98bfbff57acbfde3d37d56173a29ac4c',
+    'allowSignedRequest' => false // optional but should be set to false for non-canvas apps
+  );
+
+  $facebook = new Facebook($config);
+  $user_id = $facebook->getUser();
+?>
+<html>
+  <head></head>
+  <body>
+
+  <?php
+    if($user_id) {
+
+      // We have a user ID, so probably a logged in user.
+      // If not, we'll get an exception, which we handle below.
+      try {
+
+        $user_profile = $facebook->api('/me','GET');
+        echo "Name: " . $user_profile['name'];
+
+      } catch(FacebookApiException $e) {
+        // If the user is logged out, you can have a 
+        // user ID even though the access token is invalid.
+        // In this case, we'll get an exception, so we'll
+        // just ask the user to login again here.
+        $login_url = $facebook->getLoginUrl(); 
+        echo 'Please <a href="' . $login_url . '">login.</a>';
+        error_log($e->getType());
+        error_log($e->getMessage());
+      }   
+    } else {
+
+      // No user, print a link for the user to login
+      $login_url = $facebook->getLoginUrl();
+      echo 'Please <a href="' . $login_url . '">login.</a>';
+
+    }
+
+  ?>
+
+  </body>
+</html>
